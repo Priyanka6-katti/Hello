9014abc5b8aac4d6e3f4ca0b4d9d28b06a612f7d
Priya <Priya@Priya-PC>
2020-07-20 17:57:22 +0530

    Design

---
+++ src/hello1/Hi2.java
@@ -0,0 +1,10 @@
+package hello1;
+
+public class Hi2 {
+
+	public static void main(String[] args) {
+
+System.out.println("helloowww");
+	}
+
+}
---
+++ src/hello1/hi1.java
@@ -0,0 +1,10 @@
+package hello1;
+
+public class hi1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hiii");
+
+	}
+
+}
